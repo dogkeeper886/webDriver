661b14a4fa3df3f1561289f56534ec6ad699c750
dogkeeper886 <jack.w.tseng@outlook.com>
2020-09-14 17:20:29 +0800

    sync

---
+++ setupTenant2.py
@@ -0,0 +1,22 @@
+import slack2
+import sys
+import time
+
+if len(sys.argv) != 3:
+    print('[USERNAME][PASSWORD][PVER][IDMID][ENV]')
+    exit(1)
+
+myUname = sys.argv[1]
+myPassword = sys.argv[2]
+#myPver = sys.argv[3]
+#myIdmId = sys.argv[4]
+#myEnv = sys.argv[5]
+
+myAltobot = slack2.altobot()
+myAltobot.login(myUname, myPassword)
+myAltobot.channel('ruckus-alto-cicd')
+
+#text = '/alto-tenant setup {} {} {}'
+#myAltobot.runCommand(text.format(myPver, myIdmId, myEnv))
+#time.sleep(3)
+#myAltobot.findResult()
--- slack2.py
+++ slack2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 import selenium
 
+
 class altobot:
     browser = webdriver.Edge(executable_path='msedgedriver.exe')
     url = 'https://arris.slack.com/'
@@ -19,15 +20,27 @@ class altobot:
     def waitByid(self, id):
         try:
             print('wait element by id:', id)
-            element = WebDriverWait(self.browser, 10).until(expected_conditions.presence_of_element_located((By.ID, id)))
-        except selenium.common.exceptions.WebDriverException  as err:
+            element = WebDriverWait(self.browser, 10).until(
+                expected_conditions.presence_of_element_located((By.ID, id)))
+        except selenium.common.exceptions.WebDriverException as err:
             print(err)
+            exit(1)
+        return element
+
+    def waitBycss(self, css):
+        try:
+            print('wait element by css:', css)
+            element = WebDriverWait(self.browser, 10).until(
+                expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, css)))
+        except selenium.common.exceptions.WebDriverException as err:
+            print(err)
+            exit(1)
         return element
 
     def login(self, uName, uPassword):
-        #input email
+        # input email
         self.waitByid('email').send_keys(uName)
-        #input password
+        # input password
         self.waitByid('password').send_keys(uPassword)
         # click login
         self.waitByid('signin_btn').click()
@@ -40,12 +53,10 @@ class altobot:
                 break
 
     def runCommand(self, command):
-        c_input = self.browser.find_element_by_css_selector(
-            'div.ql-editor.ql-blank p br')
-        c_input.send_keys(command)
-        s_button = self.browser.find_element_by_css_selector(
-            'button[aria-label="Send message"]')
-        s_button.click()
+        # enter command
+        self.waitBycss('div.ql-editor.ql-blank p br').send_keys(command)
+        # send button
+        self.waitBycss('button[aria-label="Send message"]').click()
 
     def findResult(self):
         elements = self.browser.find_elements_by_css_selector(
