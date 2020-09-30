c9645e35e40514d009c969d4e7b1adbc3dbbc0d6
dogkeeper886 <jack.w.tseng@outlook.com>
2020-09-30 15:18:31 +0800

    rewrite slack

--- firefox/slack.py
+++ firefox/slack.py
@@ -1,5 +1,4 @@
 from remote import remoteFiredox
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -26,11 +25,13 @@ class slack(remoteFiredox):
 
     def runCommand(self, command):
         #c_input = self.driver.find_element_by_css_selector('div.ql-editor.ql-blank p')
-        #c_input.send_keys(command)
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.ql-editor.ql-blank'))).send_keys(command)
+        # c_input.send_keys(command)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, 'div.ql-editor.ql-blank'))).send_keys(command)
         #s_button = self.driver.find_element_by_css_selector('button[aria-label="Send message"]')
-        #s_button.click()
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Send message"]'))).click()
+        # s_button.click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, 'button[aria-label="Send message"]'))).click()
 
     def findResult(self):
         elements = self.driver.find_elements_by_css_selector(
---
+++ firefox/slackMonitor.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+import logging
+
+
+class slackMonitor:
+
+    def __init__(self, url):
+        # setup browser
+        firefox_options = webdriver.FirefoxOptions()
+        # setup remote
+        self.driver = webdriver.Remote(
+            command_executor='http://192.168.2.219:4444',
+            options=firefox_options
+        )
+        # prepare start
+        self.driver.delete_all_cookies()
+        self.driver.implicitly_wait(15)
+        logging.info('url start')
+        self.driver.get(url)
+
+    def __del__(self):
+        logging.info('remote firefox del after 10sec')
+        sleep(10)
+        self.driver.quit()
+
+    def login(self, uName, uPassword):
+        logging.info('login start')
+        # input email
+        self.driver.find_element_by_id('email').send_keys(uName)
+        # input password
+        self.driver.find_element_by_id('password').send_keys(uPassword)
+        # click login
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'signin_btn'))).click()
+
+    def channel(self, id):
+        css = 'a[data-qa-channel-sidebar-channel-id="{}"]'
+        # go to channel
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, css.format(id)))).click()
+
+    def readMessageContent(self):
+        messages = self.driver.find_elements_by_css_selector(
+            'div[data-qa="message_content"]')
+
+        for message in messages:
+            content = 'Sender Name: {} Message: {}'
+            # read message sender and message
+            sname = message.find_element_by_css_selector(
+                'a[data-qa="message_sender_name"]').text
+            smessage = message.find_element_by_css_selector(
+                'span[data-qa="message-text"]').text
+            print(content.format(sname, smessage))
+            content = {
+                "sender": sname,
+                "message": smessage,
+            }
+
