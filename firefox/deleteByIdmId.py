99b5323f36d9842383024aa5fc9247bb05d58318
dogkeeper886 <jack.w.tseng@outlook.com>
2020-10-05 14:33:36 +0800

    add send

--- firefox/deleteByIdmId.py
+++ firefox/deleteByIdmId.py
@@ -12,4 +12,5 @@ USERPASSWORD = argv[2]
 mySlack = slackMonitor('https://arris.slack.com/')
 mySlack.login(USERNAME, USERPASSWORD)
 mySlack.channel('CC04J4E3V')
-print(mySlack.readMessageContent())
+#print(mySlack.readMessageContent())
+mySlack.sendMessage()
