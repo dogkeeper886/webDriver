1751599761953793bad95304976da54eaa360d78
dogkeeper886 <jack.w.tseng@outlook.com>
2020-10-05 12:11:59 +0800

    fix syntax

--- firefox/deleteByIdmId.py
+++ firefox/deleteByIdmId.py
@@ -14,5 +14,4 @@ MSG = argv[3]
 mySlack = slackMonitor('https://arris.slack.com/')
 mySlack.login(USERNAME, USERPASSWORD)
 mySlack.channel('CC04J4E3V')
-mySlack.readMessageContent()
-mySlack.sendMessage('/alto-profile rplist dev master')
+mySlack.sendMessage(MSG)
